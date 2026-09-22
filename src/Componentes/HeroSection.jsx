fceab78e13df12473c76287bae37cdccb59fa6b1
Abduletif Abdella <abduletifabdella29@gmail.com>
2026-09-22 14:44:26 +0300

    feat: add logo image to HeroSection component

--- src/Componentes/HeroSection.jsx
+++ src/Componentes/HeroSection.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import banner from '../assets/Images/banner.png'
+import logo from '../assets/Icons/logo 1.png'
 
 function HeroSection() {
     return (
         <>
             <div style={{ backgroundImage: `url(${banner})`, backgroundSize: 'cover', backgroundPosition: 'center', height: '100vh' }}>
 
-                <div>
-                    
+                <div className='px-20 py-2'>
+                    <div><img src={logo} style={{ width: '56px', height: '56px' }} alt="Logo" /></div>
                 </div>
             </div>
         </>
