4b25c6e44e195d257859ab7ecc2121acf75bc48f
Abduletif Abdella <abduletifabdella29@gmail.com>
2026-09-26 15:42:30 +0300

    feat: add MenuItems data structure for menu functionality

---
+++ src/Componentes/Menu/MenuData.js
@@ -0,0 +1,32 @@
+import image1 from '../../assets/Images/menu-img-1.png';
+import image2 from '../../assets/Images/menu-img-2.png';
+import image3 from '../../assets/Images/menu-img-3.png';
+
+export const MenuItems = [
+    {
+        id: 1,
+        image: image1,
+        title: 'Croissant',
+        price: "₹160",
+        category: "Pastries",
+        description: "Buttery flaky pastry"
+    },
+
+    {
+        id: 2,
+        image: image2,
+        title: "Matcha Latte",
+        price: "₹240",
+        category: "Tea",
+        description: "Creamy matcha with steamed milk"
+    },
+
+    {
+        id: 3,
+        image: image3,
+        title: "Espresso",
+        price: "₹180",
+        category: "Coffee",
+        description: "Rich and concentrated coffee"
+    }
+];
